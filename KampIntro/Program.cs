431e1b87ef536ee0a955b921c2f20f2177a04689
Furkan Yılmaz <1030510165@erciyes.edu.tr>
2021-01-20 23:50:23 +0300

    Add project files.

---
+++ KampIntro/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // type safety = tip güvenliği
+            // değer tutucu , alias
+
+            string kategoriEtiketi = "Kategori";
+            Console.WriteLine(kategoriEtiketi);
+
+            /*int ogrenciSayisi = 32000;
+            double faizOrani = 1.45;*/
+            bool sistemeGirisYapmisMi = true;
+
+            if (sistemeGirisYapmisMi == true)
+            {
+                Console.WriteLine("Kullanici ayarlari butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giris yap butonu");
+            }
+
+
+            Console.WriteLine("***********Baska ornek*************");
+
+            double dolarDun = 7.55;
+            double dolarBugun = 7.45;
+
+            if (dolarDun > dolarBugun)
+            {
+                Console.WriteLine("Azalis butonu");
+            }
+            else if (dolarDun < dolarBugun)
+            {
+                Console.WriteLine("Artis butonu");
+            }
+            else
+            {
+                Console.WriteLine("Degismedi butonu");
+            }
+
+        }
+    }
+}
---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Koleksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] isimler = new string[] { "Furkan", "Eda", "Necati", "Taylan" };
+            //Console.WriteLine(isimler[0]);
+            //Console.WriteLine(isimler[1]);
+            //Console.WriteLine(isimler[2]);
+            //Console.WriteLine(isimler[3]);
+
+            List<string> isimler2 = new List<string> { "Furkan", "Eda", "Necati", "Taylan" };
+            Console.WriteLine(isimler[0]);
+            Console.WriteLine(isimler[1]);
+            Console.WriteLine(isimler[2]);
+            Console.WriteLine(isimler[3]);
+            isimler2.Add("İlker");
+            Console.WriteLine(isimler[4]);
+            Console.WriteLine(isimler[0]);
+
+        }
+    }
+}
